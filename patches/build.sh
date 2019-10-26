2b680699958ca70cd0daf52a3b356a99fe1a38d5
Michael Dreher <Michael.Dreher42@gmail.com>
2019-10-27 00:16:25 +0200

    fixed formatting, added build script

---
+++ patches/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+(cd ../../.. ; make -C . M=drivers/usb/serial)
+sudo cp ch341.ko /lib/modules/4.14.114-v7+/kernel/drivers/usb/serial/ch341.ko
+sudo rmmod ch341
+sudo modprobe ch341
+git diff ch341.c >Linux_4.14.114_ch341.patch
