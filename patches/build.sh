c87cf2b25e7a013fea6d4b65ff1a08fc626959c0
Michael Dreher <Michael.Dreher42@gmail.com>
2019-10-27 10:15:02 +0100

    added linux kernel style check of patch

--- patches/build.sh
+++ patches/build.sh
@@ -4,3 +4,4 @@ sudo cp ch341.ko /lib/modules/4.14.114-v7+/kernel/drivers/usb/serial/ch341.ko
 sudo rmmod ch341
 sudo modprobe ch341
 git diff ch341.c >Linux_4.14.114_ch341.patch
+../../../scripts/checkpatch.pl Linux_4.14.114_ch341.patch
